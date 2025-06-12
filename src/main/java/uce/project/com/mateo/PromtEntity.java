2427019ef26736dfa6e4b0e5f094b3f2ee7b3913
Mateo Curicho <m_curicho@buzz-ly.com>
2025-06-11 22:03:51 -0500

    columns in entity added

--- src/main/java/uce/project/com/mateo/PromtEntity.java
+++ src/main/java/uce/project/com/mateo/PromtEntity.java
@@ -1,4 +1,13 @@
 package uce.project.com.mateo;
 
+import uce.project.com.cat.anotations.ColumnInfo;
+import uce.project.com.cat.anotations.PrimaryKey;
+
 public class PromtEntity {
+  @ColumnInfo(name = "id")
+  @PrimaryKey(autoIncrement = true)
+  private Integer id;
+
+  @ColumnInfo(name = "prompt")
+  private String prompt;
 }
