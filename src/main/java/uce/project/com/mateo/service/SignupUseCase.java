ce368a3db98b589494dc2dded7a6585919e105b1
Mateo Curicho <mateocuricho28@gmail.com>
2025-06-23 11:07:46 -0500

    exceptions to fields added

--- src/main/java/uce/project/com/mateo/service/SignupUseCase.java
+++ src/main/java/uce/project/com/mateo/service/SignupUseCase.java
@@ -1,13 +1,34 @@
 package uce.project.com.mateo.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import uce.project.com.mateo.shared.dto.CreateUserDto;
 import uce.project.com.mateo.shared.dto.UserResponseDto;
+
+import java.util.Objects;
 
 @Service
 public class SignupUseCase {
   public UserResponseDto createUser(CreateUserDto createUserDto) {
-    
-    return new UserResponseDto();
+    if(Objects.isNull(createUserDto)) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "CreateUserDto cannot be null");
+    }
+
+    if(Objects.isNull(createUserDto.getEmail()) || createUserDto.getEmail().isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email cannot be null or empty");
+    }
+
+    if(Objects.isNull(createUserDto.getPassword()) || createUserDto.getPassword().isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password cannot be null or empty");
+    }
+
+    if(Objects.isNull(createUserDto.getName()) || createUserDto.getName().isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Name cannot be null or empty");
+    }
+
+    System.out.println(createUserDto);
+
+    return null;
   }
 }
